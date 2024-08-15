e7cfe32ba086d143fd7ecf32935ddf5d8df7a66b
Alexandr Anisimov <anisimov2002kurgan@mail.ru>
2024-08-15 17:08:16 +0500

    add Redux, add first entity

---
+++ src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
@@ -0,0 +1,20 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getCounter } from './getCounter';
+
+describe('getCounter.test', () => {
+    test('should return Counter value', () => {
+        const state: DeepPartial<StateSchema> = {
+            counter: { value: 10 },
+        };
+        expect(getCounter(state as StateSchema)).toEqual({ value: 10 });
+    });
+
+    // test('increment', () => {
+    //     expect().toEqual();
+    // });
+
+    // test('decrement', () => {
+    //     expect().toEqual();
+    // });
+});
